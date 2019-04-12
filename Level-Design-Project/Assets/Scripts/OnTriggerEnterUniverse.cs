b5df2830af8329968a3c263fd6dcbeb849ebb293
StephenHayward <stephenhaywardisme@gmail.com>
2019-04-12 11:48:45 +0100

    Add universe scripts

---
+++ Level-Design-Project/Assets/Scripts/OnTriggerEnterUniverse.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(BoxCollider))]
+public class OnTriggerEnterUniverse : MonoBehaviour
+{
+    [SerializeField]
+    UnityEvent onTriggered;
+    [SerializeField]
+    bool isTriggeredOnce;
+    [SerializeField]
+    Universes triggeredUniverse;
+
+    static UniverseController universeController;
+
+    bool isTriggered;
+
+    private void Start()
+    {
+        universeController = GameObject.FindObjectOfType<UniverseController>();
+    }
+
+    private void OnValidate()
+    {
+        GetComponent<BoxCollider>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isTriggeredOnce && isTriggered)
+            return;
+
+        if(other.tag == "Player" && triggeredUniverse == universeController.currentUniverse)
+        {
+            onTriggered.Invoke();
+        }
+    }
+}
---
+++ Level-Design-Project/Assets/Scripts/UniverseChange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UniverseChange : MonoBehaviour
+{
+    [SerializeField]
+    UnityEvent onActivate;
+    [SerializeField]
+    UnityEvent onDeactivate;
+    [SerializeField]
+    Universes universe;
+
+    private void Start()
+    {
+        UniverseController.onUniverseChanged.AddListener(OnSelectedUniverseActive);
+    }
+
+    void OnSelectedUniverseActive(int universe)
+    {
+        Universes universeEnum = (Universes)universe;
+
+        if(this.universe == universeEnum)
+        {
+            onActivate.Invoke();
+        }
+        else
+        {
+            onDeactivate.Invoke();
+        }
+    }
+}
