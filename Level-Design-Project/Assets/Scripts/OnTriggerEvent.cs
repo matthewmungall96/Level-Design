7cc75d87618623f048ab97d6909875d94068a2b8
matthewmungall96 <matthewmungall96@gmail.com>
2019-04-12 11:13:13 +0100

    Added Scripts

---
+++ Level-Design-Project/Assets/Scripts/OnTriggerEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnTriggerEvent : MonoBehaviour
+{
+    public UnityEvent onTriggered;
+    public bool trigger_once;
+    private bool isTriggered;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (trigger_once && isTriggered)
+            return;
+
+        if (other.tag == "Player")
+        {
+                onTriggered.Invoke();
+        }
+    }
+}
